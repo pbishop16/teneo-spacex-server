160fe787557601a0b742b5197f0ea46c2e27e29d
Paul Bishop <pbishop16@gmail.com>
2021-02-14 17:09:29 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const morgan = require('morgan');
+const { request, gql } = require('graphql-request');
+
+require('dotenv').config();
+
+const app = express();
+const protocol = process.env.SERVER_PROTOCOL;
+const host = process.env.SERVER_HOST;
+const port = process.env.SERVER_PORT;
+const client = process.env.CLIENT_ENDPOINT;
+
+app.use(morgan('combined'));
+
+app.get('/', (_, res) => {
+  res.send('Teneo Spacex Server');
+});
+
+const QUERY_ROCKETS = gql`
+  {
+    rockets {
+      id
+      name
+      type
+    }
+  }
+`;
+
+app.get('/rockets', async (_, res) => {
+  const data = await request(client, QUERY_ROCKETS);
+
+  res.send(data);
+});
+
+const QUERY_MISSONS = gql`
+  {
+    missions {
+      id
+      name
+      description
+    }
+  }
+`;
+
+app.get('/missions', async (_, res) => {
+  const data = await request(client, QUERY_MISSONS);
+
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`Teneo Spacex Server is listening at ${protocol}://${host}:${port}`);
+});
